1cd2edc5240746a61aec5d8931c34fb7df9bd762
rafaelbas <rafa.albuquerque314@gmail.com>
2017-05-04 21:45:50 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  param = {value: 'Rafael Albuquerque'};
+
+  constructor(private translate: TranslateService) {
+
+       // the lang to use, if the lang isn't available, it will use the current loader to get them
+      translate.use('en');
+  }
+
+  public changeLanguage(language){
+    console.log('so vem')
+    this.translate.use(language);
+  }
 }
